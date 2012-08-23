338b52864807f620066450929a382fcefc8f2097
Andreas Steffan <a.steffan@contentreich.de>
2012-08-23 09:22:09 +0200

    - initial treemap size repo webscript

---
+++ src/main/resources/alfresco/templates/webscripts/de/contentreich/info/size-treemap.get.json.ftl
@@ -0,0 +1,27 @@
+<#--
+https://github.com/mbostock/d3/wiki/Treemap-Layout#wiki-treemap
+-->
+<#escape x as jsonUtils.encodeJSONString(x)>
+
+<#macro process_entry entry>                                                                                                                                                       
+{
+"name" : "${entry.name}",
+"id" : "${entry.id]}",
+"sum" : ${entry.sum},
+<#if entry.children?has_content>
+"children" : [
+<#list entry.children as child>
+<@process_entry entry=child/>
+<#if child_has_next>,
+</#if>
+</#list>
+]
+<#else>
+"children" : []        
+</#if>
+}
+</#macro>
+                                                                                                                                                                              
+<@process_entry entry=node/>
+
+</#escape>
---
+++ src/main/resources/alfresco/templates/webscripts/de/contentreich/info/size-treemap.get.json.js
@@ -0,0 +1,49 @@
+// logger.log("xxx");
+function appendNode(folder)
+{
+	logger.log("Processing " + folder.name + ", " + folder.nodeRef.id);
+	var folderSum = 0;
+	var contentSum = 0;
+	var childFolders = folder.childFileFolders(false, true);
+	var children = [];
+	
+	for (var i=0; i < childFolders.length; i++) {
+		var child = appendNode(childFolders[i]);
+		children.push(child);
+		folderSum += child.sum; 
+	}
+
+	var childFiles = folder.childFileFolders(true, false);
+	
+	for (var i=0; i < childFiles.length; i++) {
+		contentSum += childFiles[i].size;
+	}
+	/* Special child */
+	children.push({
+		sum: contentSum,
+		name: "",
+		id:"",
+		children:[]
+	});
+	logger.log("Done " + folder.name + ", " + folder.nodeRef.id);
+	return {
+			id:folder.nodeRef.id,
+			name:folder.name,
+			children:children,
+			sum : folderSum + contentSum
+	};
+}
+
+// var node = space;
+var node = search.findNode("workspace://SpacesStore/" + url.extension);
+// status.setCode(status.STATUS_BAD_REQUEST, "Invalid");
+model.node = appendNode(node);
+
+// return tree;
+// vom letzten Node parentId, parentName null setzen - s. google doku
+/*
+for (var j=0; j<results.length; j++) {
+	print(results[j].parent + " " + results[j].node + " " + results[j].size);
+}
+*/
+// return results;
